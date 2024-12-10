9425cedeca18cf9f1d818b1057c6c113a15e487c
simhansorry <simhs@boardfnc.co.kr>
2024-12-10 17:09:12 +0900

    2024-12-10

--- src/constants/metaTag.ts
+++ src/constants/metaTag.ts
@@ -1,5 +1,6 @@
 export const PLATFORM_FAVICON = '/favicon.png';
-export const PLATFORM_NAME = '블랙 코인';
-export const PLATFORM_NAME_ENGLISH = 'BLACK COIN';
+export const PLATFORM_NAME = process.env.APP_NAME === 'platform' ? '블랙 코인' : '블랙 코인 어드민';
+export const PLATFORM_NAME_ENGLISH = process.env.APP_NAME === 'platform' ? 'BLACK COIN' : 'BLACK COIN ADMIN';
 export const PLATFORM_SPLASH_IMAGE = '/splash.png';
-export const PLATFORM_URL = 'https://black.onefabric.co.kr';
+export const PLATFORM_URL =
+  process.env.APP_NAME === 'platform' ? 'https://black-platform.onefabric.co.kr' : 'https://black.onefabric.co.kr';
