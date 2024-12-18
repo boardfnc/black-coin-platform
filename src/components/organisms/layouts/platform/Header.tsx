bec5f986523b00b97d187e219d798e5cd121915d
simhansorry <simhs@boardfnc.co.kr>
2024-12-19 00:12:04 +0900

    2024-12-18

--- src/components/organisms/layouts/platform/Header.tsx
+++ src/components/organisms/layouts/platform/Header.tsx
@@ -265,7 +265,7 @@ export default function Header() {
                 'flex items-center h-[28px] rounded-[60px] border border-gray-0 px-4 text-gray-0 font-suit-13-m-130'
               }
             >
-              로그인 {isLogin ? 'true' : 'false'}
+              로그인
             </Link>
           )}
         </div>
